3a46e02b71a20ddfba5ccaf8dd91a4731be319ba
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-05-08 14:34:28 +0300

    Configured Web Project

--- gunluk_app_web/GunlukApp.WebUI/Startup.cs
+++ gunluk_app_web/GunlukApp.WebUI/Startup.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace GunlukApp.WebUI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            IFileProvider physicalProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            services.AddSingleton<IFileProvider>(physicalProvider);
+            services.AddSession();
+            services.AddMvc();
+            services.AddDistributedMemoryCache();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +31,15 @@ namespace GunlukApp.WebUI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseSession();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Anasayfa}/{action=Index}/{id?}");
             });
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
     }
 }
