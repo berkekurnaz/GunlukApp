befc64ca18d43c8a7f039b5ed74fe6862b5662da
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-05-09 13:48:56 +0300

    Created Base Repository and User Repository

---
+++ gunluk_app_web/GunlukApp.DataAccess/Concrete/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using GunlukApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GunlukApp.DataAccess.Concrete
+{
+    public class UserRepository : BaseMongoRepository<User>
+    {
+
+        public UserRepository(string mongoDbConnectionString, string dbName, string collectionName) : base(mongoDbConnectionString,dbName,collectionName)
+        {
+
+        }
+
+    }
+}
--- gunluk_app_web/GunlukApp.WebUI/Startup.cs
+++ gunluk_app_web/GunlukApp.WebUI/Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GunlukApp.DataAccess.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
@@ -13,10 +15,19 @@ namespace GunlukApp.WebUI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             IFileProvider physicalProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            string mongoConnectionString = this.Configuration.GetConnectionString("MongoConnectionString");
+            services.AddTransient(s => new UserRepository(mongoConnectionString, "DbDiaryApp", "Users"));
 
             services.AddSingleton<IFileProvider>(physicalProvider);
             services.AddSession();
