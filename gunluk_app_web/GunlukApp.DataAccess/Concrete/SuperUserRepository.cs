1a4a8e1cf72c3b7f2aab1fe88c17e23cdae18594
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-05-12 00:52:53 +0300

    Created Admin Login Process

--- gunluk_app_web/GunlukApp.DataAccess/Concrete/SuperUserRepository.cs
+++ gunluk_app_web/GunlukApp.DataAccess/Concrete/SuperUserRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace GunlukApp.DataAccess.Concrete
 {
@@ -13,6 +15,13 @@ namespace GunlukApp.DataAccess.Concrete
         public SuperUserRepository(string mongoDbConnectionString, string dbName, string collectionName) : base(mongoDbConnectionString, dbName, collectionName)
         {
 
+        }
+
+        public SuperUser Login(SuperUser superUser)
+        {
+            var result = new SuperUser();
+            result = this.GetAll().Find(x => x.Username == superUser.Username && x.Password == superUser.Password);
+            return result;
         }
 
     }
--- gunluk_app_web/GunlukApp.WebUI/Controllers/Site/YonetimController.cs
+++ gunluk_app_web/GunlukApp.WebUI/Controllers/Site/YonetimController.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GunlukApp.DataAccess.Concrete;
+using GunlukApp.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GunlukApp.WebUI.Controllers.Site
 {
     public class YonetimController : Controller
     {
+
+        SuperUserRepository superUserRepository { get; set; }
+
+        public YonetimController(SuperUserRepository superUserRepository)
+        {
+            this.superUserRepository = superUserRepository;
+        }
 
         public IActionResult Index()
         {
@@ -17,6 +27,29 @@ namespace GunlukApp.WebUI.Controllers.Site
         public IActionResult Hata()
         {
             return View();
+        }
+
+        public IActionResult Giris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Giris(SuperUser superUser)
+        {
+            var newSuperUser = superUserRepository.Login(superUser);
+            if (newSuperUser != null)
+            {
+                HttpContext.Session.SetString("SessionSuperUsername", superUser.Username);
+                return RedirectToAction("Index", "Yonetim");
+            }
+            return View(superUser);
+        }
+
+        public IActionResult Cikis()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Anasayfa");
         }
 
     }
