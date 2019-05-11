5205f2c9ea064d26a778ecfbec7f28aebd11eb3f
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-05-12 00:25:39 +0300

    Created AdminSuperUser Pages

--- gunluk_app_web/GunlukApp.WebUI/Controllers/Site/YonetimYoneticilerController.cs
+++ gunluk_app_web/GunlukApp.WebUI/Controllers/Site/YonetimYoneticilerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using GunlukApp.DataAccess.Concrete;
 using GunlukApp.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GunlukApp.WebUI.Controllers.Site
@@ -20,31 +21,75 @@ namespace GunlukApp.WebUI.Controllers.Site
 
         public IActionResult Index()
         {
-            // Listele
-            return View();
+            var list = superUserRepository.GetAll();
+            return View(list);
         }
 
-        public IActionResult Incele()
+        public IActionResult Incele(string id)
         {
-            // Incele
-            return View();
+            var item = superUserRepository.GetById(id);
+            if (item == null)
+            {
+                return RedirectToAction("Hata", "Yonetim");
+            }
+            return View(item);
         }
 
         public IActionResult Ekle()
         {
-            // Ekle
             return View();
         }
 
-        public IActionResult Guncelle()
+        [HttpPost]
+        public IActionResult Ekle(SuperUser superUser)
         {
-            // Guncelle
+            if (ModelState.IsValid)
+            {
+                superUser.CreatedDate = DateTime.Now.ToShortDateString();
+                superUser.Photo = "Null";
+                superUserRepository.AddModel(superUser);
+                return RedirectToAction("Index");
+            }
+            return View(superUser);
+        }
+
+        public IActionResult Guncelle(string id)
+        {
+            var item = superUserRepository.GetById(id);
+            if (item == null)
+            {
+                return RedirectToAction("Hata", "Yonetim");
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(string id, SuperUser newSuperUser)
+        {
+            var item = superUserRepository.GetById(id);
+            if (ModelState.IsValid)
+            {
+                item.NameSurname = newSuperUser.NameSurname;
+                item.Email = newSuperUser.Email;
+                item.CreatedDate = newSuperUser.CreatedDate;
+                item.Photo = "Null";
+                item.Username = newSuperUser.Username;
+                item.Password = newSuperUser.Password;
+
+                superUserRepository.UpdateModel(id, item);
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
-        public IActionResult Sil()
+        [HttpPost]
+        public IActionResult Sil(string id, IFormCollection colllection)
         {
-            // Sil
+            if (ModelState.IsValid)
+            {
+                superUserRepository.DeleteModel(id);
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
