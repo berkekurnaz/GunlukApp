000e32ce0060a12da68679a2658d2a4b806b5c06
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-05-12 18:28:29 +0300

    Created Main Pages

--- gunluk_app_web/GunlukApp.WebUI/Controllers/Site/AnasayfaController.cs
+++ gunluk_app_web/GunlukApp.WebUI/Controllers/Site/AnasayfaController.cs
@@ -9,7 +9,44 @@ namespace GunlukApp.WebUI.Controllers
     public class AnasayfaController : Controller
     {
 
+        /* Sitenin Anasayfası */
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        /* Anasayfa Nedir Sayfası */
+        public IActionResult Nedir()
+        {
+            return View();
+        }
+
+        /* Anasayfa Nasıl Kullanılır Sayfası */
+        public IActionResult NasilKullanilir()
+        {
+            return View();
+        }
+
+        /* Anasayfa Takım Sayfası */
+        public IActionResult Takim()
+        {
+            return View();
+        }
+
+        /* Anasayfa Iletisim Sayfası */
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+
+        /* Anasayfa Uye Ol Sayfası */
+        public IActionResult UyeOl()
+        {
+            return View();
+        }
+
+        /* Anasayfa Giriş Yap Sayfası */
+        public IActionResult GirisYap()
         {
             return View();
         }
